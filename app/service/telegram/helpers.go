a2be96a149a163c55175e8e7577332d78d805405
rofleksey <evilmorty@yandex.ru>
2026-03-13 00:47:34 +0300

    Minor change

--- app/service/telegram/helpers.go
+++ app/service/telegram/helpers.go
@@ -7,12 +7,29 @@ import (
 )
 
 const botUsername = "maznevich_bot"
-const replyChance = 0.015
+const replyChance = 0.02
+
+var phrases = []string{
+	"1984",
+	"надо отыграться",
+	"стрим",
+	"фрик",
+	"фрича",
+	"бот",
+}
 
 var travlyaRegex = regexp.MustCompile("травл(?:я|и|ю|ей|е|ям|ями|ях|явш|емый|емую|емого)?")
 
 func needReply(text string) bool {
-	return travlyaRegex.MatchString(strings.ToLower(text)) ||
-		strings.Contains(strings.ToLower(text), botUsername) ||
+	lowerText := strings.ToLower(text)
+
+	for _, phrase := range phrases {
+		if strings.Contains(lowerText, phrase) {
+			return true
+		}
+	}
+
+	return travlyaRegex.MatchString(lowerText) ||
+		strings.Contains(lowerText, botUsername) ||
 		rand.Float32() < replyChance
 }
